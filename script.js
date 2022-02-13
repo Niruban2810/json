310cfa1ca9071bd8abe09084cd7b732953c8d194
Niruban2810 <97875800+Niruban2810@users.noreply.github.com>
2022-02-13 14:49:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var request =new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v3.1/all");
+request.send();
+request.onload=function(){
+    var data = JSON.parse(request.response);
+    console.log(data);
+    for(var i=0;i<data.length;i++){
+        console.log(`name:${data[i].name} 
+        subregion:${data[i].subregion}
+        region:${data[i].region}
+        population:${data[i].population}`);
+    }
+}
